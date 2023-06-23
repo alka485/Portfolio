80dd56d191a2b7adde712d12cc2082069931b53b
alkasah <alkasah485@gmail.com>
2023-06-23 01:35:22 -0700

    added more files in Project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,6 +4,8 @@ import ProjectCard from "./ProjectCards";
 import team from "../../Assets/Projects/Team Profile.png"
 import workday from "../../Assets/Projects/workdayscheduler.gif";
 import socialnetwork from "../../Assets/Projects/Demo.gif"
+import weather from "../../Assets/Projects/Weather Dashboard.gif";
+import password from "../../Assets/Projects/Password Generator.gif"
 function Projects() {
   return (
     <Container fluid className="project-section">
@@ -45,6 +47,26 @@ function Projects() {
               "
               ghLink="https://github.com/alka485/Social-Network-API.git"
               demoLink="https://drive.google.com/file/d/1lxJUWoTgDk3jhLlJq3c6_XCIWDs2uNQW/view"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={weather}
+              
+              title="Weather Dashboard"
+              description="This ia an app that uses the [OpenWeather API](https://openweathermap.org/api) to retrieve weather data for cities that are searched for by user. Uses `localStorage` to store any cities that have been searched for prior and allows user to call them up quickly."
+              ghLink="https://github.com/alka485/Weather-Dashboard.git"
+              demoLink="https://drive.google.com/file/d/1kCzJ58dxW25PRblytbQcy-1qVUT6dbEl/view"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={password}
+              
+              title="Password Generator"
+              description="This is an app that an employee can use to generate a random password based on criteria they've selected."
+              ghLink="https://github.com/alka485/Password-Generator.git"
+              demoLink="https://drive.google.com/file/d/12TPIzX1npT8Jmmu7UPJOQl-e7vlyK82A/view"
             />
           </Col>
 
